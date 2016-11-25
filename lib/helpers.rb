3aa05f19f1381d8cdc89eb531bee0a12d42534d9
Patrick Sinclair <metade@gmail.com>
2016-11-25 19:13:46 +0000

    Adding trainers to group run page

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,3 +1,5 @@
+require "ostruct"
+
 module Helpers
   def office_number
     "0203 432 3920"
@@ -13,5 +15,9 @@ module Helpers
 
   def login_path
     "/login"
+  end
+
+  def trainers
+    current_page.metadata[:page][:trainers].map { |data| OpenStruct.new(data) }
   end
 end
